7e1d39fbbe2d7a7945c6f8f8f5084ae13ac58a95
arpitx1996 <72219010+arpitx1996@users.noreply.github.com>
2020-10-01 22:21:34 +0530

    Create life,theuniverse.cpp
    Solution of prob 1

---
+++ life,theuniverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	while(true){
+	    int x;
+	    cin>>x;
+	    if(x==42){
+	        break;
+	    }else{
+	        cout<<x<<"\n";
+	    }
+	}
+	return 0;
+}
